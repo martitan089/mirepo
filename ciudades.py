91281f3239b06e5b72a7fa709b038f1407b05423
martin zuluaga <martincapito13@gmail.com>
2026-05-05 08:07:59 -0500

    se pusieron mas frutas y un tema

--- ciudades.py
+++ ciudades.py
@@ -1,6 +1,8 @@
 #ciudades
 
+print("aurauca")
 print("cali")
 print("ibago")
 print("medallo")
 print("boyota")
+print("Buenaaventura")
--- frutas.py
+++ frutas.py
@@ -5,3 +5,6 @@ print("manzana")
 print("uva")
 print("mandarina")
 print("naranja")
+print("luloo")
+
+print("pero es que yooo no puede dejar de decir que te amoo:3")
